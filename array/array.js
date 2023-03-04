3f11f3c24c30c785dc1e224356efd453e03ff5b3
luisgv94 <lgomero94@gmail.com>
2023-03-04 15:32:34 -0500

    Arrays implementacion Shift & Unshift

--- array/array.js
+++ array/array.js
@@ -33,13 +33,52 @@ class MyArray {
     delete this.data[this.length - 1];
     this.length--;
   }
+
+  unshift(item) {
+    if (!item) return this.length;
+    for (let i = this.length - 1; i >= 0; i--) {
+      this.data[i + 1] = this.data[i];
+    }
+    this.data[0] = item;
+    this.length++;
+    return this.length;
+  }
+
+  shift() {
+    if (this.length === 0) return undefined;
+
+    const firstItem = this.data[0];
+    for (let i = 0; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+    return firstItem;
+  }
 }
 
 const myArray = new MyArray();
+console.log('======= myArray =======');
 console.log(myArray.push('Luis'));
 console.log(myArray.push('Jose'));
 console.log(myArray.push('Fer'));
 console.log(myArray.delete(4));
-console.log(myArray.delete(4));
-console.log(myArray.delete(4));
 console.log(myArray);
+
+console.log('======= myArray2 =======');
+const myArray2 = new MyArray();
+console.log(myArray2.unshift('Platzinauta'));
+console.log(myArray2.unshift('!!!'));
+console.log(myArray2.unshift('lograste'));
+console.log(myArray2.unshift('lo'));
+console.log(myArray2);
+
+console.log('======= myArray3 =======');
+const myArray3 = new MyArray();
+myArray3.unshift('Con el');
+myArray3.unshift('Suerte');
+myArray3.unshift('desafio');
+myArray3.unshift('platzinauta');
+myArray3.unshift('Un 🐱 random en el desafío');
+console.log(myArray3.shift());
+console.log(myArray3);
