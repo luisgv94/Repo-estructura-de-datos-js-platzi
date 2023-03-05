789d87e34bc032fada252c7af1cbcdfbfd3aefa4
luisgv94 <lgomero94@gmail.com>
2023-03-05 18:21:21 -0500

    doubly implementation other methods

--- linkedList/doubly.js
+++ linkedList/doubly.js
@@ -26,6 +26,64 @@ class MyDoublyLinkedList {
 
     return this;
   }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    this.head.prev = newNode;
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    } else if (index === 0) {
+      return this.prepend(value);
+    }
+
+    let newNode = new Node(value);
+    let prevPointer = this.getTheIndex(index - 1);
+    let nextPointer = prevPointer.next;
+    newNode.prev = prevPointer;
+    newNode.next = nextPointer;
+    prevPointer.next = newNode;
+    nextPointer.prev = newNode;
+    this.length++;
+    return this;
+  }
+
+  remove(index) {
+    if (index >= this.length) {
+      console.log('index fuera de rango');
+      return;
+    }
+    let indexToRemove = this.getTheIndex(index);
+    let nextPointer = indexToRemove.next;
+    let prevPointer = indexToRemove.prev;
+    if (prevPointer && nextPointer) {
+      prevPointer.next = nextPointer;
+      nextPointer.prev = prevPointer;
+    } else if (!prevPointer) {
+      nextPointer.prev = null;
+      this.head = nextPointer;
+    } else if (!nextPointer) {
+      prevPointer.next = null;
+      this.tail = prevPointer;
+    }
+    this.length--;
+    return this;
+  }
+
+  getTheIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
 }
 
 let myDoublyLinkedList = new MyDoublyLinkedList(1);
